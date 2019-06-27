167dd5028a1bd6ac00dd754e5e34dbdd921a2b45
Tuyen Dang <tuyendq@gmail.com>
2019-06-27 08:56:37 +0700

    first commit

---
+++ compile-from-source.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+cd ~
+# Download
+URL=http://mirror.downloadvn.com/apache//httpd/httpd-2.4.39.tar.gz
+SRC_FILE=$(basename $URL)
+echo $SRC_FILE
+wget $URL
+
+#Extract source file
+SRC_DIR=$(tar -tf $SRC_FILE | head -1)
+echo "Checck SRC_DIR variable:" $SRC_DIR
+tar xzvf $SRC_FILE
+
+#Configure
+cd ~/$SRC_DIR
+./configure
+make
+sudo make install
+
